b6b5a29cae6ca5b1f0672ebdb868164e9434b209
belloibrahv <belloibrahv@gmail.com>
2024-09-18 12:37:54 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// Smooth Scrolling Function
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Mobile Menu Toggle (For small screens)
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
